e3cf5ecf5839e7a161d274e629f633d03fce87f2
space <space@BEYOND>
2019-11-16 21:26:15 +0530

    changed POST to get

--- registration_form.php
+++ registration_form.php
@@ -6,34 +6,17 @@
 </head>
 <body>
 
-    <?php if (isset($_POST['form_submitted'])): ?> //this code is executed when the form is submitted
+    <?php if (isset($_GET['form_submitted'])): ?> //this code is executed when the form is submitted
 
-        <h2>Thank You <?php echo $_POST['firstname']; ?> </h2>
+        <h2>Thank You <?php echo $_GET['firstname']; ?> </h2>
 
         <p>You have been registered as
-            <?php echo $_POST['firstname'] . ' ' . $_POST['lastname']; ?>
+            <?php echo $_GET['firstname'] . ' ' . $_GET['lastname']; ?>
         </p>
 
         <p>Go <a href="/registration_form.php">back</a> to the form</p>
 
         <?php else: ?>
-
-            <h2>Registration Form</h2>
-
-            <form action="registration_form.php" method="POST">
-
-                 First name:
-                <input type="text" name="firstname">
-
-                <br> Last name:
-                <input type="text" name="lastname">
-
-			<input type="hidden" name="form_submitted" value="1" />
-
-                <input type="submit" value="Submit">
-
-            </form>
-
       <?php endif; ? >
 </body>
 </html>
