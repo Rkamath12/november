07dc5c92a336f90100d5747bc0bf3da4fe2a125b
space <space@BEYOND>
2019-11-16 10:30:36 +0530

    changed it

--- action_page.php
+++ action_page.php
@@ -2,7 +2,7 @@
 <body>
 
 Welcome <?php echo $_POST["firstname"]; ?><br>
-Your email address is: <?php echo $_POST["lastname"]; ?>
+Last name is: <?php echo $_POST["lastname"]; ?>
 
 </body>
 </html>
